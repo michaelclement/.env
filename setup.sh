3277ca96d54f004aac12e60653d8044297bf0f30
Brandon Randle <brandonkrandle@gmail.com>
2016-11-15 07:46:22 -0600

    initialized repo with previous settings

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# A script to setup my environment settings.
+
+ln -s ~/.env/dotfiles/vimrc ~/.vimrc
+ln -s ~/.env/dotfiles/bashrc ~/.bashrc
+ln -s ~/.env/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.env/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.env/dotfiles/screenrc ~/.screenrc
+ln -s ~/.env/dotfiles/inputrc ~/.inputrc
+ln -s ~/.env/dotfiles/vim ~/.vim
